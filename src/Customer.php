4c3cbb7dadd1d7f3ee256aec8447d17156692597
yuekunchen <rosyidiyuekunchen@gmail.com>
2022-05-17 04:34:03 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+  namespace OsyKunchen\Belajar;
+
+  class Customer
+  {
+    private string $name;
+
+    public function __construct(string $name)
+    {
+      $this->name = $name;
+    }
+
+    public function sayHello(string $name): string
+    {
+      return "Hello $name, My Name is $this->name" . PHP_EOL;
+    }
+  }
